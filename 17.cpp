7aa8fb9f2c5f08d20b53eb112acee4e576b06c23
Ivan Galkin <ivan.a.galkin@googlemail.com>
2024-12-19 16:04:30 +0100

    17_1

--- 17.cpp
+++ 17.cpp
@@ -82,8 +82,7 @@ ll interpret(ll opcode, ll operand, ll pc, std::vector<ll> &output,
 
   switch (opcode) {
   case 0: {
-    ll p = std::pow(std::valarray<ll>{2}, std::valarray<ll>{combo()})[0];
-    registers[A] = registers[A] / p;
+    registers[A] = registers[A] >> combo();
     return pc + 2;
   }
   case 1: {
@@ -110,20 +109,110 @@ ll interpret(ll opcode, ll operand, ll pc, std::vector<ll> &output,
     return pc + 2;
   }
   case 6: {
-    ll p = std::pow(std::valarray<ll>{2}, std::valarray<ll>{combo()})[0];
-    registers[B] = registers[A] / p;
+    registers[B] = registers[A] >> combo();
     return pc + 2;
   }
   case 7: {
-    ll p = std::pow(std::valarray<ll>{2}, std::valarray<ll>{combo()})[0];
-    registers[C] = registers[A] / p;
+    registers[C] = registers[A] >> combo();
     return pc + 2;
   }
   }
   assert(false);
 }
 
+void translate(ll line, ll opcode, ll operand, std::ostream &os,
+               std::array<ll, 3> &ssa) {
+
+  auto SSA_READ = [&ssa](Register r) {
+    switch (r) {
+    case A:
+      return "ra" + std::to_string(ssa[A]);
+    case B:
+      return "rb" + std::to_string(ssa[B]);
+    case C:
+      return "rc" + std::to_string(ssa[C]);
+    }
+    assert(false);
+  };
+
+  auto SSA_ASSIGN = [&ssa, SSA_READ](Register r) {
+    ++ssa[r];
+    return SSA_READ(r);
+  };
+
+  auto combo = [operand, SSA_READ]() -> std::string {
+    switch (operand) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+      return std::to_string(operand);
+    case 4:
+      return SSA_READ(A);
+    case 5:
+      return SSA_READ(B);
+    case 6:
+      return SSA_READ(C);
+    case 7:
+      assert(false);
+    }
+    assert(false);
+  };
+
+  switch (opcode) {
+  case 0: {
+
+    std::ostringstream s;
+    s << " = " << SSA_READ(A) << " >> " << combo() << std::endl;
+    os << line << ":\t " << SSA_ASSIGN(A) << s.str();
+    return;
+  }
+  case 1: {
+    std::ostringstream s;
+    s << " = " << SSA_READ(B) << " ^ " << operand << std::endl;
+    os << line << ":\t " << SSA_ASSIGN(B) << s.str();
+    return;
+  }
+  case 2: {
+    std::ostringstream s;
+    s << " = " << combo() << " & 0000 0111" << std::endl;
+    os << line << ":\t " << SSA_ASSIGN(B) << s.str();
+    return;
+  }
+
+  case 3: {
+    os << line << ":\t if (" << SSA_READ(A) << " != 0) goto " << operand
+       << std::endl;
+    return;
+  }
+  case 4: {
+    std::ostringstream s;
+    s << " = " << SSA_READ(B) << " ^ " << SSA_READ(C) << std::endl;
+    os << line << ":\t " << SSA_ASSIGN(B) << s.str();
+    return;
+  }
+  case 5: {
+    os << line << ":\t print(" << combo() << " & 0000 0111)" << std::endl;
+    return;
+  }
+  case 6: {
+    std::ostringstream s;
+    s << " = " << SSA_READ(A) << " >> " << combo() << std::endl;
+    os << line << ":\t " << SSA_ASSIGN(B) << s.str();
+    return;
+  }
+  case 7: {
+    std::ostringstream s;
+    s << " = " << SSA_READ(A) << " >> " << combo() << std::endl;
+    os << line << ":\t " << SSA_ASSIGN(C) << s.str();
+    return;
+  }
+  }
+  assert(false);
+}
+
 std::vector<ll> run(const std::vector<ll> &input, std::array<ll, 3> registers) {
+
   std::vector<ll> output;
   ll pc = 0;
   while (pc < input.size()) {
@@ -134,10 +223,22 @@ std::vector<ll> run(const std::vector<ll> &input, std::array<ll, 3> registers) {
   return output;
 }
 
+void translate(const std::vector<ll> &input) {
+  std::array<ll, 3> ssa{0, 0, 0};
+  ll i = 0;
+  do {
+    ll opcode = input.at(i);
+    ll operand = input.at(i + 1);
+    translate(i, opcode, operand, std::cout, ssa);
+    i += 2;
+  } while (i < input.size());
+}
+
 std::vector<ll> run(const std::vector<ll> &input, std::array<ll, 3> registers,
                     const std::vector<ll> &reference_output) {
   std::vector<ll> output;
   ll pc = 0;
+  auto a = registers[A];
   while (pc < input.size()) {
     ll opcode = input.at(pc);
     ll operand = input.at(pc + 1);
@@ -162,24 +263,25 @@ void print(const std::vector<ll> &output) {
 
 } // namespace
 
-int main() {
+int main17() {
 
   print(run(INPUT, REGISTERS));
 
-  ll new_A = 0;
+  translate(INPUT);
+
+  ll new_A = (1ULL << (3 * (INPUT.size() - 1))) - 1;
+  myprint(new_A);
 
   while (true) {
     auto registers = REGISTERS;
     registers[A] = new_A;
+
     auto output = run(INPUT, registers, INPUT);
     if (output == INPUT) {
       myprint(new_A);
       return 0;
     }
     ++new_A;
-    if (new_A % 1000000 == 0) {
-      std::cout << new_A << " ";
-    }
   }
 
   return 0;
